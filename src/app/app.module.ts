7252d82db304c7c04cc695a771f1e2c435e54396
lluc <cashroll720@gmail.com>
2019-11-10 16:17:47 +0100

    C:\Users\lean\Desktop\Spotify\SpotifyC:\Users\lean\Desktop\Spotify\SpotifyC:\Users\lean\Desktop\Spotify\Spotimaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistaComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
